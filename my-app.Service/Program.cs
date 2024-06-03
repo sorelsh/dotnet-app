1cdd01440bbdae64d1fc22b169bbed77667c55c1
sorelsh <67732616+sorelsh@users.noreply.github.com>
2024-06-03 12:36:31 +0000

    rr

--- my-app.Service/Program.cs
+++ my-app.Service/Program.cs
@@ -26,6 +26,9 @@ app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1")
 app.UseHttpsRedirection();
 
 app.MapGet("/hello", () => "Hello, RedHat!");
+app.MapGet("/bey", () => "bey, RedHat!");
+
+
 
 app.MapControllers();
 
